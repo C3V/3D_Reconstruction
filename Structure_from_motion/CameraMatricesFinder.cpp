c55ad36f97da831e2f1433f00a9c102d037f2c1c
C3V <trevor3v@gmail.com>
2016-09-24 18:34:38 +0200

    Update CameraMatricesFinder.cpp

--- Structure_from_motion/CameraMatricesFinder.cpp
+++ Structure_from_motion/CameraMatricesFinder.cpp
@@ -34,29 +34,32 @@ void CameraMatricesFinder::convertToPoint2f(vector<DMatch> matches, vector<KeyPo
 		imgpts_left.push_back(keypoints_left[ matches[i].queryIdx ].pt );
 		imgpts_right.push_back(keypoints_right[ matches[i].trainIdx ].pt );
 	}
+	/*
 	cout<<matches[1].queryIdx <<" first queryIdx "<<matches[1].trainIdx<<" first trainIdx"<<endl;
-	cout<<imgpts_left.size()<<" =imgpts_left size; must be 34 and =imgpts_right.size()"<<endl;
+	cout<<imgpts_left.size()<<" =imgpts_left size; must be equal to number of matches and =imgpts_right.size()"<<endl;
 	cout<<imgpts_right.size()<<" =imgpts_right size"<<endl;
 	cout<<keypoints_left[matches[1].queryIdx].pt<<" first left image KeyPoint to be converted to Point2f"<<endl;
 	cout<<imgpts_left[1]<<" first converted left KeyPoint"<< endl;
-	cout<<imgpts_right[1]<<" first converted right KeyPoint"<< endl;
+	cout<<imgpts_right[1]<<" first converted right KeyPoint"<< endl;*/
 
 }
 
-Mat CameraMatricesFinder::findMatrixE(vector<DMatch> matches, vector<KeyPoint> keypoints_1, vector<KeyPoint> keypoints_2){
+Mat CameraMatricesFinder::findMatrixE(vector<DMatch> matches, vector<KeyPoint> keypoints_1, vector<KeyPoint> keypoints_2, Mat K){
 
 	convertToPoint2f(matches, keypoints_1, keypoints_2);
 
 	//obtain fundamental matrix with RANSAC algorithm (correspondences >= 8)
+	cout<<endl; cout<<"obtaining fundamental matrix:"<<endl;
 	F = findFundamentalMat(imgpts_left, imgpts_right, FM_RANSAC, 0.1, 0.99, vStatus);
+	//cout<<"fundamental matrix F must be a 3x3 Matrix:"<<endl;
+	cout<<"F= "<<F<<endl;
 
-	//TODO need the internal parameters matrix K to compute E-->calibrate camera before
 	//obtain essential matrix
-	//E = K.t() * F * K;
+	cout<<endl;cout<<"obtaining essential matrix: "<<endl;
+	E = K.t() * F * K; //according to HZ (9.12)
 
 	//[weird] program doesn't print "program ends" if I ignore the warning about returning some matrix
 	return E;
 
 	//TODO ricordati di fare release e clear di vettori e matrici come prima
 }
-
