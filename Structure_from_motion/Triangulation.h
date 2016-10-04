1d2699ff6d53c23c88fe0b35671d06768865344e
C3V <trevor3v@gmail.com>
2016-10-04 19:06:03 +0200

    Create Triangulation.h

---
+++ Structure_from_motion/Triangulation.h
@@ -0,0 +1,40 @@
+/*
+ * Triangulation.h
+ *
+ *  Created on: 27/set/2016
+ *      Author: trev
+ */
+
+#ifndef SRC_TRIANGULATION_H_
+#define SRC_TRIANGULATION_H_
+
+#include <opencv2/video/video.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/gpu/gpu.hpp>
+#include <opencv2/flann/flann.hpp>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <omp.h>
+#include <sstream>
+
+using namespace std;
+using namespace cv;
+
+class Triangulation{
+
+public:
+
+	Mat_<double> linearLSTriangulation(Point3d p, Matx34d P, Point3d p1, Matx34d P1);
+
+    double triangulatePoints(vector<KeyPoint> keypoints1, vector<KeyPoint> keypoints2,
+    		                 Mat K, Mat Kinv, Matx34d P, Matx34d P1, vector<Point3d>& pointcloud);
+
+private:
+
+
+
+};
+
+
+
+#endif /* SRC_TRIANGULATION_H_ */
