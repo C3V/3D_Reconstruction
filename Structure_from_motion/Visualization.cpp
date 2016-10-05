4eeba5dc63dc1c4d0296fb0785faa64dab0f2b71
C3V <trevor3v@gmail.com>
2016-10-05 12:11:04 +0200

    Update Visualization.cpp

--- Structure_from_motion/Visualization.cpp
+++ Structure_from_motion/Visualization.cpp
@@ -1,27 +1,24 @@
 /*
- * Visualization.cpp
+ * Visualization1.cpp
  *
- *  Created on: 04/ott/2016
+ *  Created on: 05/ott/2016
  *      Author: trev
  */
 
-
 #include "Visualization.h"
 
-void Visualization::visualize3DPoints(vector<Point3d> points, Matx34d P, Matx34d P1, Mat K){
+void Visualization1::visualize3DPoints(vector<Point3d> points, Matx34d P, Matx34d P1, Mat K){
 
-	viz::Viz3d window("Coordinate Frame");
-    window.setWindowSize(Size(650,525));
-    window.setWindowPosition(Point(150,150));
-	window.setBackgroundColor(); // black by default
+	// Create a window
+	viz::Viz3d myWindow("Coordinate Frame");
+	myWindow.setWindowSize(Size(800,600));
+    myWindow.setWindowPosition(Point(150,150));
+	myWindow.setBackgroundColor(); // black by default
 
-	cout<<"----------------------------------------------------------------------------------------------------------"<<endl;
-	cout<<endl;cout<<"populating point cloud with recovered 3D points..."<<endl;
-	cout<<"(remember that in order to use the viz module, due to the multiple installations"<<endl;
-	cout<<"of opencv I did, you need to add a new environment variable to the run configuration"<<endl;
-	cout<<"see http://stackoverflow.com/questions/27907343/error-while-loading-shared-libraries-libopencv-core-so-3-0"<<endl;
-	cout<<"for details)."<<endl;
-	cout<<"----------------------------------------------------------------------------------------------------------"<<endl;
+	// Add coordinate axes
+	//myWindow.showWidget("Coordinate Widget", viz::WCoordinateSystem());
+
+	cout<<endl;cout<<"recovering 3D points..."<<endl;
 
 	vector<Point3d> point_cloud;
 	vector<Point3d> points_ = points; //copy (to make sure the "if" does not modify points using fabs ecc.)
@@ -39,23 +36,15 @@ void Visualization::visualize3DPoints(vector<Point3d> points, Matx34d P, Matx34d
 		point_cloud.push_back(points_[i]);
 		//cout<<points_[i]<<endl;
 
-	}
+	} cout<<"done."<<endl;
 
-	cout<<endl;
-	cout<<"after check for erroneous coordinates, point_cloud has "<<point_cloud.size()<<" out of "<<points_.size()<<" points"<<endl;
-/*	cout<<endl;cout<<"point_cloud: "<<endl;
-	for(unsigned int j=0; j< point_cloud.size(); j++){
-		cout<<point_cloud[j]<<endl;
-	} */
-
-	/*
-	cout<<endl;cout<<"recovering cameras..."<<endl;
+	cout<<endl;cout << "recovering cameras... "<<endl;
 
 	Matx33d R(P(0,0), P(0,1), P(0,2),
-			  P(1,0), P(1,1), P(1,2),
+		      P(1,0), P(1,1), P(1,2),
 			  P(2,0), P(2,1), P(2,2));
 	Matx31d t(P(0,3),
-			  P(1,3),
+		      P(1,3),
 			  P(2,3));
 	cout<<"R= "<<R<<endl;cout<<" t= "<<t<<endl;
 
@@ -65,40 +54,62 @@ void Visualization::visualize3DPoints(vector<Point3d> points, Matx34d P, Matx34d
 	Matx31d t1(P1(0,3),
 			   P1(1,3),
 			   P1(2,3));
+
 	cout<<"R1= "<<R1<<endl;cout<<"t1= "<<t1<<endl;
 
-	vector<Affine3d> path;
-	path.push_back( Affine3d(R,t) );
-	path.push_back( Affine3d(R1,t1) );
-	cout<<"done"<<endl;
-*/
+    vector<Affine3d> path;
+    path.push_back( Affine3d( Mat(R), Mat(t) ) ); //need to cast to Mat (don't know why)
+    path.push_back( Affine3d( Mat(R1), Mat(t1) ) );
+    cout<<"done."<<endl;
+
 	if ( point_cloud.size() > 0 )
 	{
-	    cout<<endl;cout << "Rendering points   ... "<<endl;
+	    cout<<endl;cout<<endl;cout << "rendering points   ... "<<endl;
 		viz::WCloud cloud_widget(point_cloud, viz::Color::green());
-	    window.showWidget("point_cloud", cloud_widget);
+	    myWindow.showWidget("point_cloud", cloud_widget);
 	    cout << "done." << endl;
 	}
 	else
 	{
-		cout<<endl;cout << "Cannot render points: Empty point_cloud" << endl;
-    }
-/*
+		cout<<endl;cout << "cannot render points: empty point_cloud" << endl;
+	}
+
 	if ( path.size() > 0 )
     {
-	    cout << "Rendering Cameras  ... ";
-	    window.showWidget("cameras_frames_and_lines", viz::WTrajectory(path, viz::WTrajectory::BOTH, 0.1, viz::Color::green()));
-	    window.showWidget("cameras_frustums", viz::WTrajectoryFrustums(path, K, 0.1, viz::Color::yellow()));
-	    window.setViewerPose(path[0]);
-	    cout << "done" << endl;
+        cout<<endl;cout << "rendering Cameras  ... ";
+	    myWindow.showWidget("cameras_frames_and_lines", viz::WTrajectory(path, viz::WTrajectory::BOTH, 0.1, viz::Color::green()));
+	    myWindow.showWidget("cameras_frustums", viz::WTrajectoryFrustums(path, Matx33d(K), 0.1, viz::Color::yellow()));
+
+	    /*
+	     * hack to visualize the scene from a "simulated" point of view;
+	     * set a virtual new camera position, relative to the first camera
+	     * with matrix P[I|0], with no rotation and translation only in z
+	     * (such pose will be just behind the  other two cameras).
+	     * Then push in path and call setViewerPose() with new virtual path.
+	     */
+	    Matx33d Rg(1,0,0, //no rotation
+	    		   0,1,0,
+				   0,0,1);
+	    Matx31d tg(0,  //translate only in z
+	    		   0,
+				   -7);
+	    path.push_back( Affine3d( Mat(Rg), Mat(tg) ) );
+
+	    //comment following setViewerPose() functions to remove specific points of view
+	    cout<<"press q to exit window"<<endl;
+	    myWindow.spin(); //no point of view-->explore the 3D structure of the scene
+	    myWindow.setViewerPose(path[2]); //simulated global point of view
+	    myWindow.spin(); //no point of view
+	    myWindow.setViewerPose(path[1]); //point of view of the right camera
+	    myWindow.spin();
+
+	    cout << "done." << endl;
 	}
-	else
-	{
-	    cout << "Cannot render the cameras: Empty path" << endl;
+    else
+    {
+	    cout << "cannot render the cameras: empty path" << endl;
     }
-*/
-	cout<<"press q to exit window"<<endl;
-	window.spin(); //the window renders and starts the event loop
 
 }
+
 
