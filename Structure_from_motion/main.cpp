4d13b597db59332504e3e40170f85f42af79adf7
C3V <trevor3v@gmail.com>
2016-10-14 11:44:07 +0200

    Update main.cpp

--- Structure_from_motion/main.cpp
+++ Structure_from_motion/main.cpp
@@ -1,4 +1,3 @@
-
 /*
  * main.cpp
  *
@@ -11,9 +10,13 @@
 #include <cv.h>
 #include <highgui.h>
 #include "OpticalFlowMatcher.h"
+#include "DenseOFFeatureMatcher.h"
+#include"IncrementalReconstruction.h"
+#include "Common.h"
 #include "FindCameraMatrices.h"
 #include "Triangulation.h"
 #include <opencv2/viz.hpp> //visualization module based on VTK (requires VTK installed)
+
 #include "Visualization.h"
 
 
@@ -33,18 +36,67 @@ int main(int argc, char** argv){
 	string filename = argv[1];
 	FileStorage fs;
 	Mat K = Mat(3, 3, CV_32FC1); //camera matrix
+	Mat distortion_coeff;
 	cout<<endl;cout<<"reading camera matrix of intrinsic parameters from configuration.xml: "<<endl;
 	fs.open(filename, FileStorage::READ);
 	fs["K"]>>K;
-	cout<<"K= "<<K<<endl<<endl;
+	cout<<"K= "<<K<<endl;
+	cout<<"reading distortion coefficients:"<<endl;
+	fs["distortion_coeff"]>>distortion_coeff;
+	cout<<"distortion_coeff= "<<distortion_coeff<<endl;
 	fs.release();
 
-	//read images from folder--good frames to use->(3.11, 3.12), (2.3, 2.4)
-	Mat firstImage, secondImage;
-	firstImage = imread("images/3.11.jpg"); //2.3, 2.4
-	secondImage = imread("images/3.12.jpg");
+	//read images from folder--good frames to use:
+	//(3.11,3.12)-->notebook -  (3.15,3.16)-->ball - (5.1,5.2,5.3) ball sequence - (4.1,4.2,4.3) another ball sequence
+	//with 7.0/1/2 the incremental works(maybe!) and adds just 1 POINT (!) to baseline cloud
+	//(9.1,9.2,9.3 ) (10.1/4) (11.1/4)-->best one with 4 views
+	vector<Mat> imgs; //storage vector, pass it to IncrementalReconstruction
+	Mat firstImage, secondImage, thirdImage, fourthImage;
+	firstImage = imread("images/11.1.jpg"); imgs.push_back(firstImage);
+	secondImage = imread("images/11.2.jpg"); imgs.push_back(secondImage);
+	thirdImage = imread("images/11.3.jpg"); imgs.push_back(thirdImage);
+	fourthImage = imread("images/11.4.jpg"); imgs.push_back(fourthImage);
 
-	//compute matches between the two frames
+	{ //incremental reconstruction
+	IncrementalReconstruction i_rec;
+	bool dense = false;
+	OpticalFlowMatcher matcher;
+
+	i_rec.recover3D(imgs, matcher, dense, K, distortion_coeff);
+	}
+
+/*	//compute matches between the two frames
+	bool dense = false;
+	vector<DMatch> matches;
+	vector<KeyPoint> keypoints_left, keypoints_right;
+	map< pair<int,int>,vector<DMatch> > matches_matrix; //TODO passare al costrutt. di IncrementalR
+
+	if(dense){
+		DenseOFFeatureMatcher matcher;
+		vector<KeyPoint> keypoints1, keypoints2;
+		cout<<endl;cout<<"computing dense matching"<<endl;
+		matches = matcher.matchFeatures(firstImage, secondImage, keypoints_left, keypoints_right);
+	}
+
+	else { //sparse
+		OpticalFlowMatcher matcher;
+	    cout<<endl; cout<<"computing sparse matching:"<<endl;
+	    //matches = matcher.matchFeatures(firstImage, secondImage, &matches);
+	    matches = matcher.matchFeatures(secondImage, thirdImage, &matches);
+
+	    //display found matches
+	    matcher.displayMatches(firstImage, secondImage, matches);
+	    //matcher.displayMatches(secondImage, thirdImage, matches);
+	    //display just one match
+	    vector<DMatch> single_match;
+	    single_match.push_back(matches[4]);
+        matcher.displayMatches(firstImage, secondImage, single_match);
+
+        keypoints_left = matcher.getKeypoints_1();
+        keypoints_right = matcher.getKeypoints_2();
+	}
+*/
+	/*
 	OpticalFlowMatcher matcher;
     vector<DMatch> matches;
     cout<<endl; cout<<"computing matches:"<<endl;
@@ -62,7 +114,8 @@ int main(int argc, char** argv){
     FindCameraMatrices finder;
     vector<KeyPoint> keypoints_left, keypoints_right;
     keypoints_left = matcher.getKeypoints_1();
-    keypoints_right = matcher.getKeypoints_2();
+    keypoints_right = matcher.getKeypoints_2(); */
+/*    FindCameraMatrices finder;
     vector<KeyPoint> keypoints_left_good, keypoints_right_good;
     Matx34d P(1, 0, 0, 0, //[I|0]
               0, 1, 0, 0,
@@ -73,59 +126,41 @@ int main(int argc, char** argv){
     //Mat F = finder1.getFundamentalMat(matcher.getKeypoints_1(), matcher.getKeypoints_2(),
     		//keypoints_left_good, keypoints_right_good, matches);
     Mat Kinv(K.inv()); //inverse
-    cout<<"Kinv= "<<Kinv<<endl;
+    //cout<<"Kinv= "<<Kinv<<endl;
     vector<Point3d> outCloud; //stores found 3D points
     //also performs triangulation
+    vector<double> reproj_err;
+    vector<CloudPoint> p_cloud;
     finder.findCameraMatrices(K, Kinv, keypoints_left, keypoints_right, keypoints_left_good,
-    		keypoints_right_good, P, P1, matches, outCloud);
+    		keypoints_right_good, P, P1, matches, outCloud, p_cloud, dense, reproj_err);
     cout<<endl;cout<<"reconstructed "<<outCloud.size()<<" points out of "<<keypoints_left_good.size()<<endl;
 
-    cout<<endl;printf("help: triangulate points using matrices MPP=[I|0] and "
-    		"MPP1=[R|t] or using MPP=K[I|0] and MPP1=K[R|t]?");cout<<endl;
-
-    string filename1 = argv[2];
-    FileStorage fs1;
-    cout<<endl;cout<<"writing point cloud in output_configuration.xml"<<endl;
-	fs1.open(filename1, FileStorage::WRITE);
-	fs1<<"pointCloud"<<outCloud;
-	fs1.release();
-	cout<<"writing done"<<endl;
-
-	/*
-	{//try to use OpenCV's triangulation
-	    //camera matrices
-	    Mat_<double> M = K * Mat(P);
-	    Mat_<double> M1 = K * Mat(P1);
-
-	    vector<Point2f> ptsl, ptsr;
-	    for(unsigned int i=0; i<keypoints_left_good.size(); i++){
-	    	ptsl.push_back( keypoints_left_good[i].pt );
-	    	ptsr.push_back( keypoints_right_good[i].pt );
-	    }
-	    cout<<"ptsl.size()= "<<ptsl.size()<<" ptsr.size()= "<<ptsr.size()<<endl;
-
-	    Mat_<double> X_cv;
-	    triangulatePoints( M, M1, ptsl, ptsr, X_cv); //OpenCV triangulation
-	    cout<<"primo punto triangolato= "<<X_cv(0,0)<<endl;
-	}*/
+    //cout<<endl;printf("help: triangulate points using matrices MPP=[I|0] and "
+    //		"MPP1=[R|t] or using MPP=K[I|0] and MPP1=K[R|t]?");cout<<endl;
 
 	//show 3D results
-	Visualization display;
-	display.visualize3DPoints(outCloud, P, P1, K);
+	Visualization1 display;
+	vector<Mat_<double> > R_, t_;
+	display.visualize3DPoints(outCloud, P, P1,R_, t_, K, reproj_err);
 
     //TODO distruggere tutti gli oggetti, sennò vengono reistanziati tutte le volte, oppure
     //crearli fuori dal loop
     //clear vectors and Mats of the matcher at the end of the iteration
     //(avoid creating unnecessary objects)
-    matcher.clearFields();
+
+    //TODO implement matcher.clearFields(); inside the if(dense) else...
     cout<<endl;
     cout<<"program ends"<<endl;
 
     //TODO ?mettere tutto in una sola funzione che calcola le coordinate 3D?
 
 
-
+*/
 }
+
+
+
+
 
 
 
