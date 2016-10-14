4e01d1e92c3e6971220084cfab27f5379eafe470
C3V <trevor3v@gmail.com>
2016-10-14 11:41:49 +0200

    Update Triangulation.h

--- Structure_from_motion/Triangulation.h
+++ Structure_from_motion/Triangulation.h
@@ -1,3 +1,4 @@
+
 /*
  * Triangulation.h
  *
@@ -12,6 +13,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/gpu/gpu.hpp>
 #include <opencv2/flann/flann.hpp>
+#include "Common.h"
 
 #include <opencv2/highgui/highgui.hpp>
 #include <omp.h>
@@ -27,7 +29,8 @@ public:
 	Mat_<double> linearLSTriangulation(Point3d p, Matx34d P, Point3d p1, Matx34d P1);
 
     double triangulatePoints(vector<KeyPoint> keypoints1, vector<KeyPoint> keypoints2,
-    		                 Mat K, Mat Kinv, Matx34d P, Matx34d P1, vector<Point3d>& pointcloud);
+    		                 Mat K, Mat Kinv, Matx34d P, Matx34d P1, vector<Point3d>& pointcloud,
+							 vector<CloudPoint>& pcloud, vector<double>& reproj_err);
 
 private:
 
